4856a2b1bb89a98fa8a149c873f6d3409e1fd082
Robert Zimmerman <zim2007@gmail.com>
2019-10-14 10:19:36 -0500

    Added shipping and cart components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
-import { ProductDetailsComponent} from './product-details/product-details.component'
+import { ProductDetailsComponent} from './product-details/product-details.component';
+import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component'
 
 @NgModule({
   declarations: [
@@ -13,13 +16,18 @@ import { ProductDetailsComponent} from './product-details/product-details.compon
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent,
+    HttpClientModule,
+    ShippingComponent,
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
-      { path: 'products/:productId', component: ProductDetailsComponent}
+      { path: 'products/:productId', component: ProductDetailsComponent},
+      { path: 'cart', component: CartComponent},
+      { path: 'shipping', component: ShippingComponent}
     ]),
   ],
   providers: [],
--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -1,20 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
   items = [];
+
+  constructor(
+    private httpClient: HttpClient,
+  ){}
   
   addToCart(product){
+    console.log(this.items);
     this.items.push(product);
   }
 
   getItems(){
+    console.log(this.items);
     return this.items;
   }
 
   clearCart(){
     this.items = [];
+  }
+
+  getShippingPrices(){
+    return this.httpClient.get('/assets/shipping.json');
   }
 }
